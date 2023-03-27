526704b19817cab0462832547b11a8315fb16b07
voidful <voidful.stack@gmail.com>
2023-03-27 12:18:57 +0800

    Remove sudo from start.sh scripts

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ chown -R $USERNAME:$USERNAME /user_data
 
 screen -S code-server -dm code-server --host 0.0.0.0 --config /opt/legodev/code-server/code-server.yaml
 sleep 2
-screen -S jupyter-lab -dm sudo jupyter-lab --ip 0.0.0.0 --port ${JUPYTER_LAB_PORT} --allow-root --NotebookApp.token ${PASSWORD} --NotebookApp.password ${PASSWORD}
+screen -S jupyter-lab -dm jupyter-lab --ip 0.0.0.0 --port ${JUPYTER_LAB_PORT} --allow-root --NotebookApp.token ${PASSWORD} --NotebookApp.password ${PASSWORD}
 sleep 2
-screen -S jupyter-notebook -dm sudo jupyter-notebook --ip 0.0.0.0 --port ${JUPYTER_NOTEBOOK_PORT} --allow-root --NotebookApp.token ${PASSWORD} --NotebookApp.password ${PASSWORD}
+screen -S jupyter-notebook -dm jupyter-notebook --ip 0.0.0.0 --port ${JUPYTER_NOTEBOOK_PORT} --allow-root --NotebookApp.token ${PASSWORD} --NotebookApp.password ${PASSWORD}
 tail -f /dev/null
