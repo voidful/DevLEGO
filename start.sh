9a5d707a8a4bf678653a2d1e8d092ed94f513628
voidful <voidful.stack@gmail.com>
2024-11-16 00:02:39 +0800

    feat(start.sh): enhance conda and jupyter configuration

--- start.sh
+++ start.sh
@@ -11,13 +11,19 @@ cp /etc/skel/.profile /user_data/
 
 sudo chown -R $USERNAME:$USERNAME /opt/legodev
 source /opt/legodev/miniconda/etc/profile.d/conda.sh
-sudo echo "source $HOME/miniconda/etc/profile.d/conda.sh" >>$HOME/.bashrc
+echo "source /opt/legodev/miniconda/etc/profile.d/conda.sh" >> /user_data/.bashrc
 conda init
 conda activate base
 
+# Install nb_conda_kernels to allow Jupyter to read Conda environments
+conda install -c conda-forge nb_conda_kernels
+
+# Configure Jupyter to recognize all Conda environments under /user_data/miniconda3/envs
+export CONDA_ENVS_PATH=/user_data/miniconda3/envs
+
 screen -S code-server -dm code-server --host 0.0.0.0 --config /opt/legodev/code-server/code-server.yaml
 sleep 2
-screen -S jupyter-lab -dm jupyter-lab --ip 0.0.0.0 --port ${JUPYTER_LAB_PORT} --allow-root --NotebookApp.token ${PASSWORD} --NotebookApp.password ${PASSWORD}
+screen -S jupyter-lab -dm jupyter-lab --ip 0.0.0.0 --port ${JUPYTER_LAB_PORT} --allow-root --NotebookApp.token=${PASSWORD} --NotebookApp.password=${PASSWORD}
 sleep 2
 screen -S jupyter-notebook -dm jupyter-notebook --ip 0.0.0.0 --port ${JUPYTER_NOTEBOOK_PORT} --allow-root --NotebookApp.token=${PASSWORD} --NotebookApp.password=${PASSWORD} --NotebookApp.kernel_spec_manager_class=nb_conda_kernels.CondaKernelSpecManager
 sleep 2
