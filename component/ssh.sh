2f9b325d0708cb48d95b2d452e03bcf45ddd39e6
Eric Lam <voidful.stack@gmail.com>
2025-06-30 12:32:54 +0800

    Improve shell scripts

--- component/ssh.sh
+++ component/ssh.sh
@@ -12,7 +12,7 @@ apt install -y openssh-server
 sed -i 's/#AllowTcpForwarding no/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
 
 # Change listen port to 38005
-sed -i 's/#Port 22/Port ${SSH_PORT}/g' /etc/ssh/sshd_config
+sed -i "s/#Port 22/Port ${SSH_PORT}/g" /etc/ssh/sshd_config
 
 # Only listen on loopback
 sed -i 's/#ListenAddress 0.0.0.0/ListenAddress 127.0.0.1/g' /etc/ssh/sshd_config
--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@
 # Read ports
 . /opt/legodev/ports.sh
 
-sudo chown -R $USERNAME:$USERNAME /user_data
+sudo chown -R "${USERNAME}:${USERNAME}" /user_data
 cp /etc/skel/.bashrc /user_data/
 cp /etc/skel/.profile /user_data/
 
-sudo chown -R $USERNAME:$USERNAME /opt/legodev
+sudo chown -R "${USERNAME}:${USERNAME}" /opt/legodev
 source /opt/legodev/miniconda/etc/profile.d/conda.sh
 echo "source /opt/legodev/miniconda/etc/profile.d/conda.sh" >> /user_data/.bashrc
 conda init
@@ -23,9 +23,9 @@ export CONDA_ENVS_PATH=/user_data/miniconda3/envs
 
 screen -S code-server -dm code-server --host 0.0.0.0 --config /opt/legodev/code-server/code-server.yaml
 sleep 2
-screen -S jupyter-lab -dm jupyter-lab --ip 0.0.0.0 --port ${JUPYTER_LAB_PORT} --allow-root --NotebookApp.token=${PASSWORD} --NotebookApp.password=${PASSWORD}
+screen -S jupyter-lab -dm jupyter-lab --ip 0.0.0.0 --port "${JUPYTER_LAB_PORT}" --allow-root --NotebookApp.token="${PASSWORD}" --NotebookApp.password="${PASSWORD}"
 sleep 2
-screen -S jupyter-notebook -dm jupyter-notebook --ip 0.0.0.0 --port ${JUPYTER_NOTEBOOK_PORT} --allow-root --NotebookApp.token=${PASSWORD} --NotebookApp.password=${PASSWORD} --NotebookApp.kernel_spec_manager_class=nb_conda_kernels.CondaKernelSpecManager
+screen -S jupyter-notebook -dm jupyter-notebook --ip 0.0.0.0 --port "${JUPYTER_NOTEBOOK_PORT}" --allow-root --NotebookApp.token="${PASSWORD}" --NotebookApp.password="${PASSWORD}" --NotebookApp.kernel_spec_manager_class=nb_conda_kernels.CondaKernelSpecManager
 sleep 2
-screen -S ttyd -dm /usr/bin/ttyd -p ${TTYD_PORT} -c ${USERNAME}:${PASSWORD} bash
+screen -S ttyd -dm /usr/bin/ttyd -p "${TTYD_PORT}" -c "${USERNAME}:${PASSWORD}" bash
 tail -f /dev/null
